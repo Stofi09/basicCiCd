f30a9d5c4a6ab6068971aab69161f9b62be77981
Stofi09 <kristof.solak@gmail.com>
2023-05-19 08:10:37 +0200

    Testing negatice code coverage QGate

--- src/test/java/com/cicd/pipiline/PipilineApplicationTests.java
+++ src/test/java/com/cicd/pipiline/PipilineApplicationTests.java
@@ -10,10 +10,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class PipilineApplicationTests {
 
 	@Test
-	public void testSetHello(){
-		Hello hello = new Hello("Bye");
-		hello.setHello("Hello");
-		assertEquals("Hello",hello.getHello());
+	public void testHello(){
+		Hello hello = new Hello("hello");
+		assertEquals(hello.getHello(),"hello");
 	}
 
 }
